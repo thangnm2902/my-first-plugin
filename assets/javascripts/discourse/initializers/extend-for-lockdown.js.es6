5dd842314005b4c74f517e22bc88f26b7da7c72b
thang nguyen <thangnm2902@gmail.com>
2021-03-13 00:55:01 +0700

    111
    xx

---
+++ assets/javascripts/discourse/initializers/extend-for-lockdown.js.es6
@@ -0,0 +1,67 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { default as DiscourseURL } from "discourse/lib/url";
+import TopicStatus from "discourse/raw-views/topic-status";
+import discourseComputed from "discourse-common/utils/decorators";
+
+function initializeLockdown(api) {
+  // Intercept any HTTP 402 (Payment Required) responses for topics
+  // And redirect the client accordingly
+  api.modifyClass("model:post-stream", {
+    errorLoading(result) {
+      const status = result.jqXHR.status;
+      let response = result.jqXHR.responseJSON;
+      if (status === 402) {
+        let redirectURL =
+          response.redirect_url ||
+          this.siteSettings.category_lockdown_redirect_url;
+
+        const external = redirectURL.startsWith("http");
+        if (external) {
+          // Use location.replace so that the user can go back in one click
+          document.location.replace(redirectURL);
+        } else {
+          // Handle the redirect inside ember
+          return DiscourseURL.handleURL(redirectURL, { replaceURL: true });
+        }
+      }
+      return this._super();
+    },
+  });
+
+  api.modifyClass("component:topic-list-item", {
+    @discourseComputed
+    unboundClassNames() {
+      let classNames = this._super(...arguments) || "";
+      if (this.get("topic.is_locked_down")) {
+        classNames += " locked-down";
+      }
+
+      return classNames;
+    },
+  });
+
+  // Add an icon next to locked-down topics
+  TopicStatus.reopen({
+    @discourseComputed()
+    statuses() {
+      const results = this._super();
+      if (this.topic.is_locked_down) {
+        results.push({
+          openTag: "span",
+          closeTag: "span",
+          title: I18n.t("lockdown.topic_is_locked_down"),
+          icon: this.siteSettings.category_lockdown_list_icon,
+        });
+      }
+      return results;
+    },
+  });
+}
+
+export default {
+  name: "apply-lockdown",
+
+  initialize() {
+    withPluginApi("0.1", initializeLockdown);
+  },
+};
---
+++ plugin.rb
@@ -0,0 +1,158 @@
+# frozen_string_literal: true
+# name: discourse-category-lockdown
+# about: Set all topics in a category to redirect, unless part of a specified group
+# version: 0.1
+# authors: David Taylor
+# url: https://github.com/davidtaylorhq/discourse-category-lockdown
+
+enabled_site_setting :category_lockdown_enabled
+
+register_asset 'stylesheets/lockdown.scss'
+gem 'request_store', '1.5.0', require: true
+
+after_initialize do
+  Site.preloaded_category_custom_fields << 'redirect_url'
+  add_to_serializer(:basic_category, :redirect_url, false) do
+    object.custom_fields['redirect_url']
+  end
+
+  def before_head_close_meta(controller)
+    return "" if !controller.instance_of? TopicsController
+    return "" unless SiteSetting.category_lockdown_enabled
+    return "" unless SiteSetting.category_lockdown_allow_crawlers
+    return "" unless ::RequestStore.store[:is_crawler]
+
+    topic_view = controller.instance_variable_get(:@topic_view)
+    topic = topic_view&.topic
+    return "" if !topic
+
+    if CategoryLockdown.is_locked(controller.guardian, topic)
+      inject = []
+      if SiteSetting.category_lockdown_crawler_indicate_paywall
+        inject.push [
+          '<script type="application/ld+json">', 
+          MultiJson.dump(
+            '@context' => 'http://schema.org',
+            '@type' => 'CreativeWork',
+            'name' => topic&.title,
+            'isAccessibleForFree' => 'False',
+            'hasPart' => {
+              '@type' => 'DiscussionForumPosting',
+              'isAccessibleForFree' => 'False',
+              'cssSelector' => 'body'
+            },
+          ).gsub("</", "<\\/").html_safe, 
+          '</script>',
+        ].join("")  
+      end
+      if SiteSetting.category_lockdown_crawler_noarchive
+        inject.push '<meta name="robots" content="noarchive">'
+      end
+
+      inject.join("\n")
+    end
+  end
+
+  register_html_builder('server:before-head-close-crawler') do |controller|
+    before_head_close_meta(controller)
+  end
+
+  module ::TopicControllerLockdownExtension
+    def show
+      ::RequestStore.store[:is_crawler] = use_crawler_layout?
+      super
+    end
+  end
+
+  ::TopicsController.prepend ::TopicControllerLockdownExtension
+
+  module ::CategoryLockdown
+    def self.is_locked(guardian, topic)
+      return false if guardian.is_admin?
+
+      locked_down = topic.category&.custom_fields&.[]("lockdown_enabled") == "true"
+      return false if !locked_down
+
+      allowed_groups = topic.category&.custom_fields&.[]("lockdown_allowed_groups")
+      allowed_groups = '' if allowed_groups.nil?
+      allowed_groups = allowed_groups.split(',')
+
+      in_allowed_groups = guardian&.user&.groups&.where(name: allowed_groups)&.exists?
+
+      !in_allowed_groups
+    end
+
+    class NoAccessLocked < StandardError; end
+  end
+
+  # Restrict topic views, including RSS/Print/Search-engine
+  require_dependency 'topic_view'
+
+  module TopicViewLockdownExtension
+    def check_and_raise_exceptions(skip_staff_action)
+      super
+      return if ::RequestStore.store[:is_crawler] && SiteSetting.category_lockdown_allow_crawlers
+      raise ::CategoryLockdown::NoAccessLocked.new if SiteSetting.category_lockdown_enabled && CategoryLockdown.is_locked(@guardian, @topic)
+    end
+  end
+
+  ::TopicView.prepend TopicViewLockdownExtension
+
+  TopicList.preloaded_custom_fields << "lockdown_enabled"
+  TopicList.preloaded_custom_fields << "lockdown_allowed_groups"
+
+  require_dependency 'application_controller'
+  class ::ApplicationController
+    rescue_from ::CategoryLockdown::NoAccessLocked do
+      opts = {
+        include_ember: true
+      }
+      topic_id = params["topic_id"] || params["id"]
+      topic = Topic.find(topic_id.to_i) if topic_id
+      response = {
+        error: "Payment Required",
+      }
+      response[:redirect_url] = topic.category.custom_fields["redirect_url"] if topic
+      response[:redirect_url] ||= SiteSetting.category_lockdown_redirect_url
+      if request.format.json?
+        render_json_dump(response, status: 402)
+      else
+        redirect_to path(response[:redirect_url]), status: :moved_permanently
+      end
+
+    end
+  end
+
+  # Restrict access to posts (e.g. /raw/12/2)
+  # Also covers notifications, so people cannot 'watch' the topic
+  require_dependency 'guardian'
+  module ::PostGuardian
+    alias_method :old_can_see_post?, :can_see_post?
+
+    def can_see_post?(post)
+      return old_can_see_post?(post) unless SiteSetting.category_lockdown_enabled
+
+      return false if !old_can_see_post?(post)
+      return false if ::CategoryLockdown.is_locked(self, post.topic)
+
+      true
+    end
+  end
+
+  # Add a boolean to the topic list serializer
+  # So that we can show an icon next to each topic
+  require_dependency 'topic_list_item_serializer'
+  class ::TopicListItemSerializer
+    attributes :is_locked_down
+
+    def is_locked_down
+      ::CategoryLockdown.is_locked(scope, object)
+    end
+
+    def include_is_locked_down?
+      SiteSetting.category_lockdown_enabled
+    end
+
+  end
+
+end
